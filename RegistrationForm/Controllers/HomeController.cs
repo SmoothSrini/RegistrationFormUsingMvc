30ae08847aa7fe1b0a449805437dc5c9790ed272
SmoothSrini <55708752+SmoothSrini@users.noreply.github.com>
2020-11-21 21:33:06 +0530

    Add project files.

---
+++ RegistrationForm/Controllers/HomeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using RegistrationForm.Models;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+namespace RegistrationForm.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Result(CsRegister reg)
+        {
+            SqlConnection con = null;
+
+            string result = "";
+            try
+            {
+                con = new SqlConnection(ConfigurationManager.ConnectionStrings["connstring"].ToString());
+                SqlCommand cmd = new SqlCommand("sp_Insert_Details", con);
+                cmd.CommandType = CommandType.StoredProcedure;
+                //cmd.Parameters.AddWithValue("@CustomerID", 0);  
+                cmd.Parameters.AddWithValue("@Name", reg.Name);
+                cmd.Parameters.AddWithValue("@Age", reg.Age);
+                cmd.Parameters.AddWithValue("@Mobileno", reg.Phoneno);
+                cmd.Parameters.AddWithValue("@EmailID", reg.Email);
+                con.Open();
+                result = cmd.ExecuteScalar().ToString();
+            }
+            catch(Exception ex)
+            {
+               
+            }
+            finally
+            {
+                con.Close();
+            }
+            return View();
+        }
+    }
+}
---
+++ RegistrationForm/Models/CsRegister.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace RegistrationForm.Models
+{
+    public class CsRegister
+    {
+        [Required(ErrorMessage = "Please Enter Name")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Age")]
+        [Range(18,90)]
+        public int Age { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Phone No")]
+        public int Phoneno { get; set; }
+
+        [Required(ErrorMessage = "Please Enter Email")]
+        [EmailAddress]
+        public string Email { get; set; }
+    }
+}
